09e06c7b56258acf9682842d01ddbda984c5ee57
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-10-19 18:28:16 +0600

    contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import useAuth from '../../hooks/useAuth';
 import './Contact.css';
 
 const Contact = () => {
+    const { user } = useAuth();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         <div className='contact-us mt-5 py-5'>
             <h1 className="text-center">Contact Us</h1>
+            <div className="row">
+                <div className="col-md-6">
+                    <form className="contact-form" onSubmit={handleSubmit(onSubmit)}>
+                        <input defaultValue={user.displayName} {...register("name", { required: true })} />
+                        <input placeholder="Your Email" defaultValue={user.email} {...register("email", { required: true })} />
+                        {errors.email && <span className='error'>This field is required</span>}
+                        <input placeholder="Your Address" {...register("address")} />
+                        <input placeholder="City" {...register("city", { required: true })} />
+                        <input type="submit" />
+                    </form>
+                </div>
+                <div className="col-md-6"></div>
+            </div>
         </div>
     );
 };
